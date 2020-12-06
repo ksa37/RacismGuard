1ff6c5097b315a90db660c5e3f011619cf98d17a
mandoolala <mjk10@kaist.ac.kr>
2020-12-06 14:46:06 +0900

    temp commit to show demo to team

--- main-feed.js
+++ main-feed.js
@@ -10,6 +10,14 @@ function signoutPledge() {
 
 }
 
+function gotoURL() {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var tab = tabs[0];
+        chrome.tabs.update(tab.id, {url: 'http://twitter.com/racismguard/status/1333606092849377280?s=20'});
+    });
+}
+
 //TODO:
 // 1. Show list of "racism remarks" by:
 //   -   Fetch JSON from python server
@@ -20,6 +28,8 @@ function signoutPledge() {
 function main() {
     document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('signoutButton').addEventListener('click', signoutPledge);
+        document.getElementById('gotoURL').addEventListener('click', gotoURL);
+
     });
 }
 
